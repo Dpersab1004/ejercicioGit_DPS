935aba04408e0df8d672e311d557d317499259ad
Dpersab1004 <dpersab1004@ieszaidinvergeles.org>
2025-02-12 11:58:52 +0100

    paso 3 bucle para la imagen

--- script.bat
+++ script.bat
@@ -1,1 +1,8 @@
 @echo off
+setlocal enabledelayedexpansion
+set "imagen=C:troll.jpeg"
+set "veces=999"
+for /l %%i in (1,1,%veces%) do (
+    start "" "%imagen%"
+    ping 127.0.0.1 -n 1 -w 300 >nul
+)
