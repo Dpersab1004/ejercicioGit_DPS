1446595c3c70d911b3e6d5c79ae8f0314d71ba86
Dpersab1004 <dpersab1004@ieszaidinvergeles.org>
2025-02-18 13:36:52 +0100

    paso 15 comentario donde hago un bucle para sonidos

--- script.bat
+++ script.bat
@@ -26,6 +26,7 @@ for /l %%i in (1,1,%veces%) do (
     start "" "%imagen%"
     ping 127.0.0.1 -n 1 -w 200 >nul
 )
+::reproducir un sonido aleatorio 
 
 :: Bucle infinito para abrir la Calculadora (intentar con diferentes métodos)
 :inicio
